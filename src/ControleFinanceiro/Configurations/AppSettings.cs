f2c879eaf388577c369ca504bd4d6d14a9803ad8
PauloAlves8039 <pj38alves@gmail.com>
2024-02-22 10:40:32 -0300

    feat: criando banco de dados e configurações

---
+++ src/ControleFinanceiro/Configurations/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleFinanceiro.Configurations
+{
+    public class AppSettings
+    {
+        private static string DatabaseName = "ControleFinanceiro.db";
+        private static string DatabaseDirectory = FileSystem.AppDataDirectory;
+        public static string DatabasePath = Path.Combine(DatabaseDirectory, DatabaseName);
+    }
+}
---
+++ src/ControleFinanceiro/Repositories/TransactionRepository.cs
@@ -0,0 +1,45 @@
+﻿using ControleFinanceiro.Repositories.Interfaces;
+using LiteDB;
+using ControleFinanceiro.Models;
+
+namespace ControleFinanceiro.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly LiteDatabase _database;
+        private readonly string _collectionName = "transactions";
+
+        public TransactionRepository(LiteDatabase database)
+        {
+            _database = database;
+        }
+
+        public List<Transaction> GetAll()
+        {
+            return _database
+                .GetCollection<Transaction>(_collectionName)
+                .Query()
+                .OrderByDescending(a => a.Date)
+                .ToList();
+        }
+
+        public void Add(Transaction transaction)
+        {
+            var collection = _database.GetCollection<Transaction>(_collectionName);
+            collection.Insert(transaction);
+            collection.EnsureIndex(a => a.Date);
+        }
+
+        public void Update(Transaction transaction)
+        {
+            var collection = _database.GetCollection<Transaction>(_collectionName);
+            collection.Update(transaction);
+        }
+
+        public void Delete(Transaction transaction)
+        {
+            var collection = _database.GetCollection<Transaction>(_collectionName);
+            collection.Delete(transaction.Id);
+        }
+    }
+}
