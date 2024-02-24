10da71f2981deb2a5d5ba73edc7bdbfd6b3c3f72
PauloAlves8039 <pj38alves@gmail.com>
2024-02-24 10:25:30 -0300

    feat: adicionando views e suas configurações

--- src/ControleFinanceiro/App.xaml.cs
+++ src/ControleFinanceiro/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace ControleFinanceiro
+﻿using ControleFinanceiro.Views;
+
+namespace ControleFinanceiro
 {
     public partial class App : Application
     {
@@ -6,7 +8,7 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new TransactionList();
         }
     }
 }
---
+++ src/ControleFinanceiro/Views/TransactionList.xaml.cs
@@ -0,0 +1,9 @@
+namespace ControleFinanceiro.Views;
+
+public partial class TransactionList : ContentPage
+{
+	public TransactionList()
+	{
+		InitializeComponent();
+	}
+}
