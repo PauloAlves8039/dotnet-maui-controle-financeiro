a4d2b6b85ef982d81850063578fca36695fb5b78
PauloAlves8039 <pj38alves@gmail.com>
2024-02-28 12:30:19 -0300

    feat: criação das validações dos campos

--- src/ControleFinanceiro/Views/TransactionAdd.xaml.cs
+++ src/ControleFinanceiro/Views/TransactionAdd.xaml.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace ControleFinanceiro.Views;
 
 public partial class TransactionAdd : ContentPage
@@ -10,5 +12,42 @@ public partial class TransactionAdd : ContentPage
     private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
     {
 		Navigation.PopModalAsync();
+    }
+
+    private void OnButtonClicked_Save(object sender, EventArgs e)
+    {
+
+    }
+
+    private bool IsValidData() 
+    {
+        bool valid = true;
+        double result;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (string.IsNullOrEmpty(EntryName.Text) || string.IsNullOrWhiteSpace(EntryName.Text)) 
+        {
+            stringBuilder.AppendLine("O campo 'Nome' deve ser preenchido!");
+            valid = false;
+        }
+
+        if (string.IsNullOrEmpty(EntryValue.Text) || string.IsNullOrWhiteSpace(EntryValue.Text))
+        {
+            stringBuilder.AppendLine("O campo 'Valor' deve ser preenchido!");
+            valid = false;
+        }
+
+        if (string.IsNullOrEmpty(EntryValue.Text) || double.TryParse(EntryValue.Text, out result)) 
+        {
+            stringBuilder.AppendLine("O campo 'Valor' é inválido!");
+            valid = false;
+        }
+
+        if (valid == false) 
+        {
+            LabelError.Text = stringBuilder.ToString();
+        }
+
+        return valid;
     }
 }
