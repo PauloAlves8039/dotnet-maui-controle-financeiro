bed7f3af9cfa7f2812e7f487a087095334549506
PauloAlves8039 <pj38alves@gmail.com>
2024-03-01 10:22:30 -0300

    feat: listagem dos registros das transações

--- src/ControleFinanceiro/Views/TransactionList.xaml.cs
+++ src/ControleFinanceiro/Views/TransactionList.xaml.cs
@@ -1,15 +1,25 @@
+using ControleFinanceiro.Repositories.Interfaces;
+
 namespace ControleFinanceiro.Views;
 
 public partial class TransactionList : ContentPage
 {
     private TransactionAdd _transactionAdd;
     private TransactionEdit _transactionEdit;
+    private ITransactionRepository _repository;
 
-    public TransactionList(TransactionAdd transactionAdd, TransactionEdit transactionEdit)
+    public TransactionList(
+        TransactionAdd transactionAdd, 
+        TransactionEdit transactionEdit, 
+        ITransactionRepository repository)
 	{
         _transactionAdd = transactionAdd;
         _transactionEdit = transactionEdit;
+        _repository = repository;
+
         InitializeComponent();
+
+        CollectionViewTransactions.ItemsSource = _repository.GetAll();
 	}
 
 	private void OnButtonClicked_To_TransactionAdd(object sender, EventArgs args) 
