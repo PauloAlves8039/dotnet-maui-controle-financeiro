a267f65a0222c994a2cdfeb2b227f28ab0d160eb
PauloAlves8039 <pj38alves@gmail.com>
2024-03-02 09:45:43 -0300

    feat: configurada aberta de nova instância

--- src/ControleFinanceiro/Views/TransactionList.xaml.cs
+++ src/ControleFinanceiro/Views/TransactionList.xaml.cs
@@ -4,17 +4,10 @@ namespace ControleFinanceiro.Views;
 
 public partial class TransactionList : ContentPage
 {
-    private TransactionAdd _transactionAdd;
-    private TransactionEdit _transactionEdit;
     private ITransactionRepository _repository;
 
-    public TransactionList(
-        TransactionAdd transactionAdd, 
-        TransactionEdit transactionEdit, 
-        ITransactionRepository repository)
+    public TransactionList(ITransactionRepository repository)
 	{
-        _transactionAdd = transactionAdd;
-        _transactionEdit = transactionEdit;
         _repository = repository;
 
         InitializeComponent();
@@ -24,11 +17,13 @@ public partial class TransactionList : ContentPage
 
 	private void OnButtonClicked_To_TransactionAdd(object sender, EventArgs args) 
 	{
-        Navigation.PushModalAsync(_transactionAdd);
+        var transactionAdd = Handler.MauiContext.Services.GetService<TransactionAdd>();
+        Navigation.PushModalAsync(transactionAdd);
     }
 
     private void OnButtonClicked_To_TransactionEdit(object sender, EventArgs e)
     {
-        Navigation.PushModalAsync(_transactionEdit);
+        var transactionEdit = Handler.MauiContext.Services.GetService<TransactionEdit>();
+        Navigation.PushModalAsync(transactionEdit);
     }
 }
