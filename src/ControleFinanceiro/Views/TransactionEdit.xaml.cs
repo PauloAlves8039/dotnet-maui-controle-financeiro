3ad4edb8434d384bc7c645933108c26052c0465d
PauloAlves8039 <pj38alves@gmail.com>
2024-03-04 20:15:29 -0300

    feat: salvando a edição dos dados

--- src/ControleFinanceiro/Views/TransactionEdit.xaml.cs
+++ src/ControleFinanceiro/Views/TransactionEdit.xaml.cs
@@ -1,16 +1,21 @@
+using CommunityToolkit.Mvvm.Messaging;
 using ControleFinanceiro.Models;
 using ControleFinanceiro.Models.Enums;
+using ControleFinanceiro.Repositories.Interfaces;
+using System.Text;
 
 namespace ControleFinanceiro.Views;
 
 public partial class TransactionEdit : ContentPage
 {
     private Transaction _transaction;
+    private ITransactionRepository _repository;
 
-    public TransactionEdit()
+    public TransactionEdit(ITransactionRepository repository)
 	{
 		InitializeComponent();
-	}
+        _repository = repository;
+    }
 
     public void SetTransactionToEdit(Transaction transaction) 
     {
@@ -30,8 +35,68 @@ public partial class TransactionEdit : ContentPage
         EntryValue.Text = transaction.Value.ToString();
     }
 
-    private void TapGestureRecognizer_Tapped(object sender, TappedEventArgs e)
+    private void TapGestureRecognizerTapped_ToClose(object sender, TappedEventArgs e)
     {
         Navigation.PopModalAsync();
+    }
+
+    private void OnButtonClicked_Save(object sender, EventArgs e)
+    {
+        if (IsValidData() == false) 
+        {
+            return;
+        }
+            
+        SaveTransactionInDatabase();
+
+        Navigation.PopModalAsync();
+        WeakReferenceMessenger.Default.Send<string>(string.Empty);
+    }
+
+    private bool IsValidData() 
+    {
+        bool valid = true;
+        double result;
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (string.IsNullOrEmpty(EntryName.Text) || string.IsNullOrWhiteSpace(EntryName.Text))
+        {
+            stringBuilder.AppendLine("O campo 'Nome' deve ser preenchido!");
+            valid = false;
+        }
+
+        if (string.IsNullOrEmpty(EntryValue.Text) || string.IsNullOrWhiteSpace(EntryValue.Text))
+        {
+            stringBuilder.AppendLine("O campo 'Valor' deve ser preenchido!");
+            valid = false;
+        }
+
+        if (!string.IsNullOrEmpty(EntryValue.Text) && !double.TryParse(EntryValue.Text, out result))
+        {
+            stringBuilder.AppendLine("O campo 'Valor' é inválido!");
+            valid = false;
+        }
+
+        if (valid == false)
+        {
+            LabelError.IsVisible = true;
+            LabelError.Text = stringBuilder.ToString();
+        }
+
+        return valid;
+    }
+
+    private void SaveTransactionInDatabase()
+    {
+        Transaction transaction = new Transaction()
+        {
+            Id = _transaction.Id,
+            Type = RadioIncome.IsChecked ? TransactionType.Income : TransactionType.Expense,
+            Name = EntryName.Text,
+            Date = DatePickerDate.Date,
+            Value = double.Parse(EntryValue.Text)
+        };
+
+        _repository.Update(transaction);
     }
 }
