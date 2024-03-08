9d6f94738d34ffa5c612213bbceac1833fb10aae
PauloAlves8039 <pj38alves@gmail.com>
2024-03-08 14:27:27 -0300

    feat: converte valores das transações

---
+++ src/ControleFinanceiro/Libraries/Converters/TransactionValueConverter.cs
@@ -0,0 +1,33 @@
+﻿using ControleFinanceiro.Models;
+using ControleFinanceiro.Models.Enums;
+using System.Globalization;
+
+namespace ControleFinanceiro.Libraries.Converters
+{
+    public class TransactionValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+
+            if (transaction == null) 
+            {
+                return "";
+            }
+
+            if (transaction.Type == TransactionType.Income)
+            {
+                return transaction.Value.ToString("C");
+            }
+            else 
+            {
+                return $"- {transaction.Value.ToString("C")}";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
